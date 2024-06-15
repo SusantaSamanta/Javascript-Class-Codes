a2f42ad72eabf22f12a6d85a04a71279cb472c95
Susanta Samanta <160142120+SusantaSamanta@users.noreply.github.com>
2024-06-15 23:55:14 +0530

    3rd Update on Class 3 (function).js

--- Class 3/Class 3 (function).js
+++ Class 3/Class 3 (function).js
@@ -122,6 +122,41 @@ const fastupper = (str) => {
       copystr += str[i].toLowerCase()
   }
 }
-fastupper('helLo sUSAnta sAmAnta')
+fastupper('hElLo sUSAnta sAmAnta')
 console.log(copystr)
 
+//            Sentence editing 
+
+const alphabet = (a) =>{
+  if(  a =='a' || a=='b' || a == 'c' || a =='d' || a=='e' || a == 'f' || a =='g' || a=='h' || a == 'i' || a =='j' || a=='k' || a == 'l' || a =='p' || a=='q' || a == 'r' || a =='s' || a=='t' || a == 'u' || a =='v' || a=='w' || a == 'x' || a =='y' || a == 'z' || a =='A' || a=='B' || a == 'C' || a =='D' || a=='E' || a == 'F' || a =='G' || a=='H' || a == 'I' || a =='J' || a=='K' || a == 'L' || a =='P' || a=='Q' || a == 'R' || a =='S' || a=='T' || a == 'U' || a =='V' || a=='W' || a == 'X' || a =='Y' || a == 'Z')
+   return true
+   else
+   return false 
+}
+let copystr2="";
+const fastupper2 = (str) => {
+  n = str.length
+  for(i=0;i<n;i++){
+    if(i==0)
+      copystr2 += str[i].toUpperCase()
+    else if("<" ==str[i]){
+      copystr2 += str[i] = " "
+      copystr2 += str[i+1].toUpperCase()
+      i=i+1
+    }
+    else if("." ==str[i] && alphabet(str[i+1]) ){
+      copystr2 += str[i] = ". "
+      copystr2 += str[i+1].toUpperCase()
+      i=i+1
+    }
+    else if("." ==str[i] && " " == str[i+1]){
+      copystr2 += str[i] = ". "
+      copystr2 += str[i+2].toUpperCase()
+      i=i+2
+    }
+    else
+      copystr2 += str[i].toLowerCase()
+  }
+}
+fastupper2('hElLo <sUSAnta <sAmAnta. weLcome tO our weBsiTe. thankS for visiting ouR WebSite.wE always Help yoU.')
+console.log(copystr2)
