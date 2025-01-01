1ba8afa2ca8da5354782baf74a8a711f56aa38a8
Susanta Samanta <160142120+SusantaSamanta@users.noreply.github.com>
2025-01-01 21:29:49 +0530

    Create 1. Variables.js

---
+++ Js class by thapa technical/1. Variables.js
@@ -0,0 +1,17 @@
+        //   cameCase to write variables 
+var myName = "Susanta";  
+console.log(myName);
+
+        //   Rules to write variables name
+//var 6Six = 'Six';
+// => We can't use a number as first in the variable name 
+var _6Six = 'Six';
+//var -6Six = 'Six';
+//var _-Six = 'Six';
+var _Six = 'Six';
+console.log(_Six);
+// => in case of Symbol only _ use in various or any other Symbols are not use in variables.
+
+
+
+
